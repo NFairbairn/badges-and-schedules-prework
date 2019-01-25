463e0039ce6488c58909d332223c4b183400a268
Noah Fairbairn <noahfairbairn@gmail.com>
2019-01-25 18:31:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,5 +9,5 @@ def batch_badge_creator (attendees)
 end
 
 def assign_rooms (attendees)
-  attendees.each_with_index {|attendee, index| "Hello, #{attendee}! You'll be assigned to room #{index}!"}
+  attendees.each_with_index {|attendee, index| "Hello, "+ attendee +"! You'll be assigned to room "+ index +"!"}
 end
