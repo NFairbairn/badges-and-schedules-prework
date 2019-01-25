618d03b7f86aa569b64609e071e47c731237d042
Noah Fairbairn <noahfairbairn@gmail.com>
2019-01-25 18:47:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ def assign_rooms (attendees)
   attendees.map.with_index {|attendee, index| "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"}
 end
 
-def printer
+def printer (attendees)
   batch_badge_creator(attendees)
   assign_rooms(attendees)
 end
