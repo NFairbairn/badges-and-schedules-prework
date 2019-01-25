a56fa026d7eae72b31595c51b17dde66499cabe5
Noah Fairbairn <noahfairbairn@gmail.com>
2019-01-25 18:29:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,7 @@ end
 def batch_badge_creator (attendees)
   attendees.collect {|attendee|"Hello, my name is " + attendee + "."}
 end
+
+def assign_rooms (attendees)
+  attendees.each_with_index {|attendee, index| "Hello, #{attendee}! Your room is #{index}! "}
+end
